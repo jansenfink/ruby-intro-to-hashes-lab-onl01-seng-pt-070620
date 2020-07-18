769b4a0c26d3ed2cbd6a52a886840052ee9cf01f
Jansen Fink <jansenfink@charter.net>
2020-07-18 21:05:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,10 +11,8 @@ def monopoly
 end
 
 def monopoly_with_second_tier
-  monopoly = 
-{:railroads => {},
-	:names => {}  
-	}
+monopoly = {:railroads => {:pieces => 4, :names => {} ,:rent_in_dollars =>{}}
+}
 
 end
 
